4c15f7f0c81545a4ee2d9a779bc2cdd25c025e7e
Gustavo Ruske <gustavoruske@gmail.com>
2019-05-22 20:00:06 -0300

    ajustado para caso necessario aguardar o modal de loading fechar

--- scraper_utils.js
+++ scraper_utils.js
@@ -1,3 +1,6 @@
+const { PendingXHR } = require('pending-xhr-puppeteer');
+const cssSelectors = require('./css_selectors');
+
 async function getTextByCssSelector(page, selector) {
     const element = await page.$(selector);
     return await (await element.getProperty('textContent')).jsonValue();
@@ -9,12 +12,17 @@ async function clickButton(page, selector) {
     
     await page
         .evaluate((cssSelector) => {document.querySelector(cssSelector).click()}, selector);
+
+    await waitForAllXhrFinished(page, true)
 }
 
-async function waitForAllXhrFinished(pendingXHR, page, selector) {
-    if (Number(pendingXHR.pendingXhrCount()) != 0) await pendingXHR.waitForAllXhrFinished();
-    if (selector) {
-        await page.waitForFunction("document.querySelector('"+selector+"').getAttribute('style') == 'display: none;'");
+async function waitForAllXhrFinished(page, waitLoadingModalHide) {
+    let pendingXHR = await new PendingXHR(page);
+    if (Number(pendingXHR.pendingXhrCount()) != 0) {
+        await pendingXHR.waitForAllXhrFinished();
+    }
+    if (waitLoadingModalHide) {
+        await page.waitForFunction("document.querySelector('"+cssSelectors.ATTRACTION.loading+"').getAttribute('style') == 'display: none;'");
     }
 }
 
