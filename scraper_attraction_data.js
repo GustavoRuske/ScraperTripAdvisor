d2ad8e231bba2278d94c7e361dbb82f5aa14f9e8
Gustavo Ruske <gustavoruske@gmail.com>
2019-05-25 11:55:08 -0300

    Alterado para pegar os dados do atrativo na mesma pagina e nao abrir uma nova

--- scraper_attraction_data.js
+++ scraper_attraction_data.js
@@ -1,18 +1,17 @@
 const puppeteer = require('puppeteer');
 const scraper_utils = require('./scraper_utils');
 const cssSelectors = require('./css_selectors');
-// const database = require('./database');
-const { PendingXHR } = require('pending-xhr-puppeteer');
 const attraction = {}
 attraction.reviews = []
 
 async function scrape_data(urlPage) {
     try {
-        await scrape_attraction_info(urlPage)
         let browser = await puppeteer.launch({headless: true});
         let page = await browser.newPage();
 
         await page.goto(urlPage);
+
+        await scrape_attraction_info(page)
 
         for(period in cssSelectors.PERIOD) {
             await scraper_utils.clickButton(page, cssSelectors.PERIOD[period]);
@@ -25,12 +24,18 @@ async function scrape_data(urlPage) {
             }
             await scraper_utils.clickButton(page, cssSelectors.PERIOD[period]);
         }
+
+        await browser.close();
+        let attraction_return = attraction
+        attraction = {}
+        attraction.reviews = []
+
+        return attraction_return
+
     } catch (error) {
         console.log("erro ao buscar os dados na url: " + urlPage);
         console.log("error: " + error);
-    } finally {
         await browser.close();
-        return attraction
     }
 }
 
@@ -52,20 +57,11 @@ async function scrape_reviews(page, period, traveler) {
     attraction.reviews.push(reviews)
 }
 
-async function scrape_attraction_info(urlPage) {
-    let browser = await puppeteer.launch({headless: true});
-    let page = await browser.newPage();
-    let pendingXHR = await new PendingXHR(page);
-
-    await page.goto(urlPage);
-    await pendingXHR.waitForAllXhrFinished(pendingXHR);
-
+async function scrape_attraction_info(page) {
     for(selector in cssSelectors.ATTRACTION) {
         let content = String(await scraper_utils.getTextByCssSelector(page, cssSelectors.ATTRACTION[selector]))
         attraction[selector] = selector == "category" ? content.split(", ") : content
     }
-    await browser.close();
 }
 
-// scrape_data('https://www.tripadvisor.com.br/Attraction_Review-g2344316-d3893494-Reviews-Parque_Aquatico_Cascaneia-Gaspar_State_of_Santa_Catarina.html')
-scrape_data("https://www.tripadvisor.com.br/Attraction_Review-g680306-d8048445-Reviews-Passeio_San_Miguel-Balneario_Camboriu_State_of_Santa_Catarina.html")
+module.exports.scrape_data = scrape_data;
